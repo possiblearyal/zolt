f8dcb6ee981781a594be11188c6e2207fd95e1f3
Rijan Dhakal <rijandhakal0619@gmail.com>
2025-12-10 00:13:27 +0545

    setup : basic electron-forge setup with ts

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+// src/index.ts
+import { app, BrowserWindow, ipcMain } from "electron";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow(): void {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "dist/preload.js"), // Now this works
+    },
+  });
+
+  // Load your frontend
+  //   const frontendPath = path.join(__dirname, "../frontend/index.html");
+  //   mainWindow.loadFile(frontendPath);
+
+  // loading the url because got some problems while loading from dir ( temp view )
+  mainWindow.loadURL("http://localhost:5173");
+
+  // Open DevTools in development
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
